0508f2f738cfdb1c791efff5dd454805a1647e6e
olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2026-01-21 19:45:45 -0800

    Remove duplicate lambda_role from backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,21 +1,3 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "stc_lambda_role"
-
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
-}
-
-data "aws_iam_policy_document" "lambda_assume" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 resource "aws_lambda_function" "ai_chat_fn" {
   function_name = "stc_ai_chat"
   role          = aws_iam_role.lambda_role.arn
@@ -26,5 +8,3 @@ resource "aws_lambda_function" "ai_chat_fn" {
   filename         = "${path.module}/ai_chat.zip"
   source_code_hash = filebase64sha256("${path.module}/ai_chat.zip")
 }
-
-
--- infra/iam.tf
+++ infra/iam.tf
@@ -20,12 +20,25 @@ resource "aws_iam_role_policy_attachment" "lambda_logging" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "stc_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes = [name]
+    ignore_changes  = [name]
   }
 }
