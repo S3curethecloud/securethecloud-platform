6a40d2a0158754230dbf47ba69738bfff23c432a
olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2026-01-21 20:12:48 -0800

    Restore IAM assume policy data block

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,12 +1,13 @@
+# Lambda Execution Role
 resource "aws_iam_role" "lambda_exec" {
   name = "stc-lambda-exec-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
-        Action = "sts:AssumeRole",
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -20,12 +21,32 @@ resource "aws_iam_role" "lambda_exec" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Data policy document for Lambda assume role
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# General Lambda Role (for ai_chat_fn, etc.)
 resource "aws_iam_role" "lambda_role" {
-  name = "stc_lambda_role"
+  name               = "stc_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes = [name]
+    ignore_changes  = [name]
   }
 }
