6d9d70a50026a88143199c0d9817b6054456e355
olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2026-01-21 20:09:58 -0800

    Add lifecycle block to IAM roles to prevent duplicate creation errors

--- infra/iam.tf
+++ infra/iam.tf
@@ -20,24 +20,8 @@ resource "aws_iam_role" "lambda_exec" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_logging" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-data "aws_iam_policy_document" "lambda_assume" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 resource "aws_iam_role" "lambda_role" {
-  name               = "stc_lambda_role"
+  name = "stc_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
 
   lifecycle {
