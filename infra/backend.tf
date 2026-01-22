b74f73f9ee709b80d51d4107f08b76b15196df76
olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2026-01-21 19:32:23 -0800

    Remove duplicate IAM role definitions from backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -16,11 +16,6 @@ data "aws_iam_policy_document" "lambda_assume" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_exec" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_lambda_function" "ai_chat_fn" {
   function_name = "stc_ai_chat"
   role          = aws_iam_role.lambda_role.arn
